9cc0ebd61eb7df8ee08f6616fa508b8eed779940
sahar <saharnikkhah81@gmail.com>
2024-11-23 12:28:02 +0330

    second correct part one

---
+++ src/pages/User.tsx
@@ -0,0 +1,5 @@
+export default function User() {
+  return (
+    <div className="w-full flex justify-between items-center lg:gap-36 lg:py-18 md:gap-2 gap-2 md:px-8 md:py-16 px-4 py-12 max-w-[1140px] xl:px-0 xl:mx-auto"></div>
+  );
+}
