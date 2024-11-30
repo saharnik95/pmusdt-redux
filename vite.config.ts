2e9630bd0b02135142baa40383a7c7bf2288c5f2
sahar <saharnikkhah81@gmail.com>
2024-11-30 11:58:17 +0330

    correction

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
-import path from "path";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "/src",
     },
   },
   css: {
